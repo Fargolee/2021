99ac66114623e8fb40f357ff3b700fe20b233564
Fargolee <775977072@qq.com>
2022-01-18 19:33:07 +0800

    2022-1-18

---
+++ PytestFrameworks/test_example.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Author: Lee
+Descripttion: RTMart
+Date: 2022-01-18 18:35:27
+'''
+'''
+Pytest编写规则:
+
+测试文件以test_开头（以_test为结尾）
+测试的类以Test开头；
+测试的方法以test_开头
+断言使用基本的assert
+'''
+import pytest
+
+def count_num(a: list) -> int:
+    return len(a)
+
+def test_count():
+    assert count_num([1,2,3]) ==3
