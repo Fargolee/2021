b37b33c65d4f870f71703abfc41fd7e61eace6ed
lee <775977072@qq.com>
2022-05-26 23:43:49 +0800

    2022-05-26 23:43:45

--- PytestDemo/run.py
+++ PytestDemo/run.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding:utf-8 -*-
-#@Time  : 2022/5/26 23:25
-#@File  : run.py
+# @Time  : 2022/5/26 23:25
+# @File  : run.py
+import pytest
+
+if __name__ == '__main__':
+    pytest.main(['-vs'])
---
+++ PytestDemo/test_api.py
@@ -0,0 +1,23 @@
+'''
+Author: Lee
+Descripttion: Myhome
+Date: 2022-05-26 22:22:35
+'''
+import pytest
+
+
+class TestShiCi:
+    def test_baili(self):
+        print('去岁江南见雪时，月底梅花发')
+        # raise Exception('抛出异常')
+
+    def test_dufu(self):
+        print('星垂平野阔，月涌大江流')
+
+
+    def test_gaoqi(self):
+        print('从今四海永为家，不用长江限南北。')
+
+
+# if __name__ == "__main__":
+#     pytest.main()
